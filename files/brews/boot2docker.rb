d33806ae6fbf26727672372353385ea9425df952
Jamie Connolly <jamie@jamieconnolly.com>
2014-05-30 12:35:46 +0100

    Add in the docker::package class that installs the docker package

---
+++ files/brews/boot2docker.rb
@@ -0,0 +1,32 @@
+require "formula"
+
+class Boot2docker < Formula
+  homepage "https://github.com/boot2docker/boot2docker-cli"
+  url "https://github.com/boot2docker/boot2docker-cli.git", :tag => "v0.9.2"
+
+  version "0.9.2-boxen1"
+
+  bottle do
+  end
+
+  depends_on "boxen/brews/docker" => :recommended
+  depends_on "go" => :build
+
+  def install
+    (buildpath + "src/github.com/boot2docker/boot2docker-cli").install Dir[buildpath/"*"]
+
+    cd "src/github.com/boot2docker/boot2docker-cli" do
+      ENV["GOPATH"] = buildpath
+      system "go", "get", "-d"
+
+      ENV["GIT_DIR"] = cached_download/".git"
+      system "make", "goinstall"
+    end
+
+    bin.install "bin/boot2docker-cli" => "boot2docker"
+  end
+
+  test do
+    system "#{bin}/boot2docker", "version"
+  end
+end
---
+++ files/brews/docker.rb
@@ -0,0 +1,40 @@
+require "formula"
+
+class Docker < Formula
+  homepage "http://docker.io/"
+  url "https://github.com/dotcloud/docker.git", :tag => "v0.11.1"
+
+  version "0.11.1-boxen1"
+
+  bottle do
+    sha1 "2a97dab4bb3109c57ed99144feb3d2bad6588b2c" => :mavericks
+    sha1 "0013849c9ab2b85374a944e25c42f974ee986911" => :mountain_lion
+    sha1 "a9a6021d388f1b004f262977968ed727c4aea022" => :lion
+  end
+
+  option "without-completions", "Disable bash/zsh completions"
+
+  depends_on "go" => :build
+
+  def upstream_version
+    version.to_s.split('-').first
+  end
+
+  def install
+    ENV["GIT_DIR"] = cached_download/".git"
+    ENV["AUTO_GOPATH"] = "1"
+    ENV["DOCKER_CLIENTONLY"] = "1"
+
+    system "hack/make.sh", "dynbinary"
+    bin.install "bundles/#{upstream_version}/dynbinary/docker-#{upstream_version}" => "docker"
+
+    if build.with? "completions"
+      bash_completion.install "contrib/completion/bash/docker"
+      zsh_completion.install "contrib/completion/zsh/_docker"
+    end
+  end
+
+  test do
+    system "#{bin}/docker", "--version"
+  end
+end
